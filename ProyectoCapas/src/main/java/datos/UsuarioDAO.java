9796dd45a78117472415a33a95d1afce3ebd2f23
Esduardo Del Aguila <daguilae@yahoo.com>
2021-02-18 13:22:55 -0600

    Actualizar de metodo personaDAO_query ahora regresa objetos

--- ProyectoCapas/src/main/java/datos/UsuarioDAO.java
+++ ProyectoCapas/src/main/java/datos/UsuarioDAO.java
@@ -4,23 +4,25 @@
  * and open the template in the editor.
  */
 package datos;
+
 import domain.Usuario;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 
 /**
  *
  * @author visitante
  */
 public class UsuarioDAO {
+
     private static final String SQL_SELECT = "SELECT id_usuario, username, password FROM usuario";
     private static final String SQL_INSERT = "INSERT INTO usuario(username, password) VALUES(?, ?)";
     private static final String SQL_UPDATE = "UPDATE usuario SET username=?, password=? WHERE id_usuario = ?";
     private static final String SQL_DELETE = "DELETE FROM usuario WHERE id_usuario=?";
-    
-    public List<Usuario> select(){
+    private static final String SQL_QUERY = "SELECT id_usuario, username, password FROM usuario WHERE id_usuario = ?";
+
+    public List<Usuario> select() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -30,32 +32,31 @@ public class UsuarioDAO {
             conn = Conexion.getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
             rs = stmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int id_usuario = rs.getInt("id_usuario");
                 String username = rs.getString("username");
                 String password = rs.getString("password");
-                
+
                 usuario = new Usuario();
                 usuario.setId_usuario(id_usuario);
                 usuario.setUsername(username);
                 usuario.setPassword(password);
-                
+
                 usuarios.add(usuario);
             }
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }
-        finally{
+        } finally {
             Conexion.close(rs);
             Conexion.close(stmt);
             Conexion.close(conn);
         }
-        
+
         return usuarios;
     }
-    
-    public int insert(Usuario usuario){
+
+    public int insert(Usuario usuario) {
         Connection conn = null;
         PreparedStatement stmt = null;
         int rows = 0;
@@ -64,22 +65,21 @@ public class UsuarioDAO {
             stmt = conn.prepareStatement(SQL_INSERT);
             stmt.setString(1, usuario.getUsername());
             stmt.setString(2, usuario.getPassword());
-            
+
             System.out.println("ejecutando query:" + SQL_INSERT);
             rows = stmt.executeUpdate();
             System.out.println("Registros afectados:" + rows);
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }
-        finally{
+        } finally {
             Conexion.close(stmt);
             Conexion.close(conn);
         }
-        
+
         return rows;
     }
-    
-    public int update(Usuario usuario){
+
+    public int update(Usuario usuario) {
         Connection conn = null;
         PreparedStatement stmt = null;
         int rows = 0;
@@ -90,26 +90,25 @@ public class UsuarioDAO {
             stmt.setString(1, usuario.getUsername());
             stmt.setString(2, usuario.getPassword());
             stmt.setInt(3, usuario.getId_usuario());
-            
+
             rows = stmt.executeUpdate();
             System.out.println("Registros actualizado:" + rows);
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }
-        finally{
+        } finally {
             Conexion.close(stmt);
             Conexion.close(conn);
         }
-        
+
         return rows;
     }
-    
-    public int delete(Usuario usuario){
+
+    public int delete(Usuario usuario) {
         Connection conn = null;
         PreparedStatement stmt = null;
         int rows = 0;
-        
+
         try {
             conn = Conexion.getConnection();
             System.out.println("Ejecutando query:" + SQL_DELETE);
@@ -119,13 +118,47 @@ public class UsuarioDAO {
             System.out.println("Registros eliminados:" + rows);
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }
-        finally{
+        } finally {
             Conexion.close(stmt);
             Conexion.close(conn);
         }
-        
+
         return rows;
     }
-    
+
+    public int query(Usuario usuario) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int rows = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            System.out.println("Ejecutando query:" + SQL_QUERY);
+            stmt = conn.prepareStatement(SQL_QUERY);
+            stmt.setInt(1, usuario.getId_usuario());
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id_usuario = rs.getInt("id_usuario");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                
+                usuario = new Usuario();
+                usuario.setId_usuario(id_usuario);
+                usuario.setUsername(username);
+                usuario.setPassword(password);
+                
+                rows++;
+            }
+            System.out.println("Registros buscado:" + usuario);
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+
+        return rows;
+    }
 }
--- ProyectoCapas/src/main/java/vista/ManejoUsuarios.java
+++ ProyectoCapas/src/main/java/vista/ManejoUsuarios.java
@@ -1,28 +1,36 @@
 package vista;
 
 import datos.UsuarioDAO;
+import domain.Persona;
 import domain.Usuario;
 import java.util.List;
 
 public class ManejoUsuarios {
+
     public static void main(String[] args) {
         UsuarioDAO usuarioDAO = new UsuarioDAO();
-        List<Usuario> usuarios = usuarioDAO.select();
-        for(Usuario usuario: usuarios){
-            System.out.println("Usuario:" + usuario);
-        }     
-      
-        
         //Insertamos un nuevo usuario
- //       Usuario usuario = new Usuario("Luis.Siliezar", "123456");
- //       usuarioDAO.insert(usuario);
+        Usuario usuarioInsertar = new Usuario("Luis.Perez", "guate");
+        usuarioDAO.insert(usuarioInsertar);
+        
 
         //Modificamos un usuario existente
-//        Usuario usuario = new Usuario(3, "carlos.juarez","456");
-//        usuarioDAO.update(usuario);
+//        Usuario usuarioModificar = new Usuario(3, "carlos.juarez","456");
+//        usuarioDAO.update(usuarioModificar);
+//
+//      Prueba delete
+//        usuarioDAO.delete(new Usuario(2));
+//
+//        // Prueba query
+//        Usuario usuarioConsultar = new Usuario();
+        //      usuarioConsultar.setId_usuario(1);
+        //      usuarioDAO.query(usuarioConsultar);        
+        //       
+        List<Usuario> usuarios = usuarioDAO.select();
+        for (Usuario usuario : usuarios) {
+            System.out.println("Usuario:" + usuario);
+        }
+        //Ejecutando el listado de usuarios
 
-        usuarioDAO.delete(new Usuario(1));
-          //Ejecutando el listado de usuarios
-   
     }
 }
